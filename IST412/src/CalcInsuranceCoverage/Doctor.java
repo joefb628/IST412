61b81a2df8df7ae40e37d3d802e22e80c1bb58db
Bryan <Bryan@BryanXPS13>
2019-09-22 22:27:25 -0400

    calculate insurance coverage package

---
+++ IST412/src/CalcInsuranceCoverage/Doctor.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CalcInsuranceCoverage;
+
+/**
+ *
+ * @author Bryan
+ */
+public class Doctor {
+    private int doctorID;
+    private String patientID;
+    private String insuranceID;
+    private String treatment;
+    
+    //constructor for doctor model class
+    public Doctor(){
+        
+    }
+
+    public int calculatePriceOfTreatment(String patientID, String treatment, String insuranceID){
+        return 0;
+    }
+    
+    /**
+     * @return the doctorID
+     */
+    public int getDoctorID() {
+        return doctorID;
+    }
+
+    /**
+     * @param doctorID the doctorID to set
+     */
+    public void setDoctorID(int doctorID) {
+        this.doctorID = doctorID;
+    }
+
+    /**
+     * @return the patientID
+     */
+    public String getPatientID() {
+        return patientID;
+    }
+
+    /**
+     * @param patientID the patientID to set
+     */
+    public void setPatientID(String patientID) {
+        this.patientID = patientID;
+    }
+
+    /**
+     * @return the treatment
+     */
+    public String getTreatment() {
+        return treatment;
+    }
+
+    /**
+     * @param treatment the treatment to set
+     */
+    public void setTreatment(String treatment) {
+        this.treatment = treatment;
+    }
+
+    /**
+     * @return the insuranceID
+     */
+    public String getInsuranceID() {
+        return insuranceID;
+    }
+
+    /**
+     * @param insuranceID the insuranceID to set
+     */
+    public void setInsuranceID(String insuranceID) {
+        this.insuranceID = insuranceID;
+    }
+    
+}
